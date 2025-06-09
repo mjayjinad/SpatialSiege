090dff3b3fbbf0354bd59969b58a32a557fa925c
Abdulmaliq Jinad <abdulmaliq.jinad@gmail.com>
2025-06-09 00:08:14 -0400

    SpaceShip Spawner

---
+++ Assets/Scripts/ShipSpawner.cs
@@ -0,0 +1,24 @@
+using Meta.XR.MRUtilityKit;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject spaceShip;
+    [SerializeField] private float normalOffset;
+
+
+    void Start()
+    {
+        MRUK.Instance.SceneLoadedEvent.AddListener(SpawnSpaceShip);
+    }
+
+    private void SpawnSpaceShip()
+    {
+        MRUKRoom room = MRUK.Instance.GetCurrentRoom();
+
+        Vector3 spawnPos = room.CeilingAnchor.transform.position * normalOffset;
+
+        Instantiate(spaceShip, spawnPos, Quaternion.identity);
+    }
+}
